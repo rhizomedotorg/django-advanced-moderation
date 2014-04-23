4d2983a7480c063ab865eab9cb67c7722c694124
Rhizome.org <smeisburger@gmail.com>
2014-04-23 15:32:27 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-advanced-moderation',
+      version='0.10',
+      description='',
+      author='Scott Meisburger',
+      author_email='smeisburger@gmail.com',
+      url='https://github.com/rhizomedotorg/django-advanced-moderation',
+      packages=['advancedmod'],
+     )
